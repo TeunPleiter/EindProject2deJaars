e445ebadeb5146c2e51ff90f550f083e5a59da3e
ConnerWitTweeduizend <97093629@st.deltion.nl>
2024-07-03 15:06:46 +0200

    divs verwidjerd

--- eindopdracht-app/resources/views/layouts/pages/favorites.blade.php
+++ eindopdracht-app/resources/views/layouts/pages/favorites.blade.php
@@ -6,19 +6,13 @@
 </x-slot>
 
 @section('content')
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                @foreach ($favorites as $favorite)
-                <li>{{ $favorite->movie->title }}</li>
-                <a href="{{ route('removefavorite', $favorite->movie->id) }}" style="text-decoration: none;">
-                    <button>REMOVE FAVORITE</button>
-                </a>
-                @endforeach
-            </div>
-        </div>
-    </div>
-</div>
+@foreach ($favorites as $favorite)
+<li>{{ $favorite->movie->title }}</li>
+<a href="{{ route('removefavorite', $favorite->movie->id) }}" style="text-decoration: none;">
+    <button>REMOVE FAVORITE</button>
+</a>
+<br>
+<img src="{{ route('getPoster', $favorite->movie->id) }}" alt="Movie Poster" style="max-width: 100%; height: auto;">
+@endforeach
 
 @endsection

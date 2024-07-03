9ecca6976c84b1155278bbf7cfa489ac313bc429
ConnerWitTweeduizend <97093629@st.deltion.nl>
2024-07-03 15:29:43 +0200

    Update favorites.blade.php

--- eindopdracht-app/resources/views/layouts/pages/favorites.blade.php
+++ eindopdracht-app/resources/views/layouts/pages/favorites.blade.php
@@ -1,10 +1,4 @@
 @extends('layouts.default')
-<x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-        {{ __('Dashboard') }}
-    </h2>
-</x-slot>
-
 @section('content')
 @foreach ($favorites as $favorite)
 <li>{{ $favorite->movie->title }}</li>
